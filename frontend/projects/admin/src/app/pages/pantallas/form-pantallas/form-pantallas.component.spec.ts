42eb612d63480d0b2dc8f8502293a6ab9e28325e
Marcelo <mabc@live.cl>
2020-02-25 14:00:53 -0300

    Actualización de form-pantallas.component.spect.js

--- frontend/projects/admin/src/app/pages/pantallas/form-pantallas/form-pantallas.component.spec.ts
+++ frontend/projects/admin/src/app/pages/pantallas/form-pantallas/form-pantallas.component.spec.ts
@@ -1,7 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormPantallasComponent } from './form-pantallas.component';
-import {} from 'jest';
 
 describe('FormPantallasComponent', () => {
   let component: FormPantallasComponent;
